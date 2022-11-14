939efb5769e3ec0c8887e8f6150f6eec0fc0ac27
MabelG27 <mabelgarcia_d27@hotmail.com>
2022-11-13 20:24:51 -0400

    Subiendo Compilador, para tarea de compiladores.

---
+++ src/Traductor/CTranslator.java
@@ -0,0 +1,76 @@
+package Traductor;
+
+import java.io.*;
+
+public class CTranslator {
+
+    public CTranslator() {
+
+    }//CTranslator constructor
+
+    public void CTranslate(String line, File outfile) {
+
+	try {
+	    PrintWriter writer = new PrintWriter(new FileOutputStream(outfile, true));//, "UTF-8"); // Permite escribir un nuevo archivo
+	    writer.println(line);                                                                   // Imprime la nueva linea en el nuevo archivo
+
+	    writer.close();                                                                         // cierra el nuevo archivo
+	} catch (Exception IOException) {
+	    System.out.println("Some sort of IO error here");
+	}//try-catch
+
+    }//CTranslate
+
+
+    public void addJavaHeader(String name, File outfile) {
+
+	try {
+	    PrintWriter writer = new PrintWriter(new FileOutputStream(outfile, true));             // deDeclara un nuevo PrintWriter, también permite escribir un archivo nuevo
+
+	    /*
+	      Adds java headers
+	    */
+
+	    writer.println("public class " + name + " {");
+	    writer.println("    public static void main(String[] args) {");
+	    writer.println();
+
+	    writer.close();
+	} catch (Exception IOException) {
+	    System.out.println("Some sort of IO error here");
+	}
+	    
+    }//addJavaHeader
+
+    public void CstringTrans(String line, File outfile) {
+
+	try {
+	    PrintWriter writer = new PrintWriter(new FileOutputStream(outfile, true));
+
+	    String[] printStatement = line.split("<<");	    
+	    int lastentry = printStatement.length - 1;
+	    if (printStatement[lastentry].equals(" endl;")) {
+		writer.print("System.out.println(");
+		lastentry--;
+	    } else {
+		writer.print("System.out.print(");
+	    }//if
+	    
+	    for (int i = 1; i < lastentry - 1; i++) {
+		writer.print(printStatement[i] + " + ");
+	    }//for
+	    if (lastentry > 0) { //retirar ; de la última entrada
+		String newline = printStatement[lastentry].replaceAll("\\;","");
+		writer.println(newline + ");");
+	    } else {
+		writer.println(");");
+	    }//if-else
+
+	    writer.close();
+	} catch (Exception IOException) {                                                        
+	    System.out.println("Some sort of IO error here");
+	}
+	
+    }//CTranslator
+
+}
---
+++ src/Traductor/JTranslator.java
@@ -0,0 +1,87 @@
+package Traductor;
+
+import java.io.*;
+
+public class JTranslator {
+
+    public JTranslator() {
+
+    }
+
+    public void JTranslate(String line, File outfile) {
+
+	try {
+	    PrintWriter writer = new PrintWriter(new FileOutputStream(outfile, true));//, "UTF-8"); //permite escribir en un nuevo archivo
+	    writer.println(line);  // imprime la línea de código en el nuevo archivo
+
+	    writer.close();  // cierra el archivo nuevo
+	} catch (Exception IOException) {
+	    System.out.println("Some sort of IO error here");
+	}
+
+    }
+
+// Agrega un encabezado C++ al nuevo archivo
+    public void addCPPHeader(File outfile) {
+
+	try {
+            // declara al escritor como un nuevo PrintWriter, nos permite escribir en un nuevo archivo
+	    PrintWriter writer = new PrintWriter(new FileOutputStream(outfile, true)); 
+
+      /*
+     Agrega encabezados de C++
+      */
+	    writer.println("#include <iostream>");
+	    writer.println("#include <string>");
+	    writer.println("using namespace std;");
+	    writer.println("int main() {");
+
+	    writer.close();
+	} catch (Exception IOException) {
+	    System.out.println("Some sort of IO error here");
+	}
+
+    }
+
+    //método que traduce sentencias de impresión de Java a C++
+    public void JstringTrans(String line, File outfile) {
+
+	try {
+	    PrintWriter writer = new PrintWriter(new FileOutputStream(outfile, true));
+
+	    String[] printStatement = line.split("\\(");  // divide el printStatement después de (
+	    char[] systemSt = printStatement[0].toCharArray();//establece la primera parte de split en systemSt
+	    char[] printSt = printStatement[1].toCharArray();  // establece la segunda parte de la división en pintereSt
+	    int sysLength = systemSt.length; // crea sysLength para obtener la longitud de la primera parte de la división
+	    boolean endl = false;  // Establece endl = a falsa
+
+       // si la longitud de systemSt -2 es l y systemSt -1 es n, declara endl como verdadero
+	    if (systemSt[sysLength - 2] == 'l' && systemSt[sysLength - 1] == 'n') {
+		endl = true;
+	    }
+
+	    writer.print("cout <<");// escribr cout << en el nuevoarchivo
+
+	    //se repite siempre que i sea menor que la longitud de printSt -2 y lee cada parte de la matriz de caracteres
+	    for (int i = 0; i < (printSt.length - 2); i++) {
+
+		if (printSt[i] == '+') {  //si el código encuentra un +, lo reemplazará con <<
+		    writer.print("<<");
+		} else {                     // de lo contrario, continuará
+		    writer.print(printSt[i]);
+		}
+	    }
+
+	    if (endl) { // si endl es verdadero, el código agregará << endl; hasta el final de la línea.
+		writer.print("<< endl");
+	    }
+
+	    writer.println(";");
+
+
+	    writer.close(); //termina con la escritura en un nuevo archivo
+	} catch (Exception IOException) {                                                        
+	    System.out.println("Some sort of IO error here");
+	}
+    }
+}
